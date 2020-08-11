ac3e800bba302fdeb5b0e531796056ee0a80ec23
humble-barnacle001 <barnaclenshade@protonmail.com>
2020-08-12 01:34:37 +0530

    Added weather upto 3 days; UI Improvements

--- src/js/weather.js
+++ src/js/weather.js
@@ -5,8 +5,8 @@ class Weather {
     }
 
     async getWeather() {
-        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${this.api_key}&q=${this.city}`);
-        // const response = await fetch(`../testresJSON/forecast0${Math.ceil(Math.random() * 9)}.json?q=${this.city}`);
+        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${this.api_key}&q=${this.city}&days=3`);
+        // const response = await fetch(`testresJSON/forecast0${Math.ceil(Math.random() * 9)}.json?q=${this.city}`);
         //USED FOR TESTING PURPOSES WITH DOWNLOADED JSON FILES WITH "Live Server"
         const responseData = await response.json();
         return responseData;
