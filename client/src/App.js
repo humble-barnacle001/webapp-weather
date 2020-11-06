e7c0641e57c90af913e26f1f41e5939fcbac758e
humble-barnacle001 <barnaclenshade@protonmail.com>
2020-11-06 14:04:53 +0530

    Attempt move to react

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+// import "./App.css";
+
+function App() {
+    return (
+        <div className='content-wrapper'>
+            <div className='container'>
+                <div>
+                    <img src={logo} className='m-auto' alt='logo' width='100' />
+                    <p>Under Construction.............</p>
+                    <a href='https://webapp-weather.netlify.app/'>
+                        Visit this for the working version
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
